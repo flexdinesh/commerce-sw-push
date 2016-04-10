8e14a201ef06dc4f834fc2684dd8413c297af108
dinesh <flexdinesh@gmail.com>
2016-04-10 16:41:48 +0530

    added bower

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use("/", express.static(__dirname + '/static/'));
 app.use("/css", express.static(__dirname + '/static/css'));
 app.use("/js", express.static(__dirname + '/static/js'));
 app.use("/images", express.static(__dirname + '/static/images'));
+app.use("/bower_components", express.static(__dirname + '/static/bower_components'));
 app.use("/logs", express.static(__dirname + '/logs'));
 
 /*Logger Declaration*/
